245344bfd7070154b0b6ccf1ea39b4a923ad2470
matheus pedroso <pedrosom482@gmail.com>
2025-09-14 00:29:56 -0300

    TODO: Add skills section

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,10 +1,11 @@
 import { Carousel, Divider, Layout, Menu } from "antd";
 import { DiJava } from "react-icons/di";
 import { FaGithub, FaReact } from "react-icons/fa";
-import { SiJunit5, SiPostgresql, SiSpring, SiTypescript } from "react-icons/si";
+import { SiPostgresql, SiSpring, SiTypescript } from "react-icons/si";
 import { colors } from "../../assets/styles/colors";
 import type { PortfolioController } from "./hook/usePortfolio";
 import { CarouselCard, Container, HeaderContacts, HelloCard, StyledButton } from "./styles/PortfolioStyles";
+import { motion } from "framer-motion";
 
 interface PortfolioProps {
     controller: PortfolioController
@@ -28,7 +29,7 @@ export default function Portfolio({ controller }: PortfolioProps) {
                     width: '100%',
                     zIndex: '1000',
                 }}
-                >
+            >
 
                 <Menu
                     mode="horizontal"
@@ -48,42 +49,50 @@ export default function Portfolio({ controller }: PortfolioProps) {
 
             </Header>
 
-            <Content
-                style={{
-                    width: '100%',
-                    height: 'calc(100vh - 64px)',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    paddingTop: '64px'
-                }}
-                id="home"
+            <motion.div
+                initial={{ opacity: 0, scale: 1.1 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.8, ease: "easeIn" }}
             >
-                <HelloCard>
-                    <div>
-                        <h1>Matheus Pedroso</h1>
-                        <p style={{ fontFamily: 'Instrument', fontStyle: 'italic', textAlign: 'center' }}>Full Stack Software Developer </p>
-                    </div>
-                </HelloCard>
+                <Content
+                    style={{
+                        width: '100%',
+                        height: 'calc(100vh - 64px)',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        paddingTop: '64px'
+                    }}
+                    id="home"
+                >
+                    <HelloCard>
+                        <div>
+                            <h1>Matheus Pedroso</h1>
+                            <p style={{ fontFamily: 'Instrument', fontStyle: 'italic', textAlign: 'center' }}>Full Stack Software Developer </p>
+                        </div>
+                    </HelloCard>
 
-                <Carousel autoplay style={{ width: '100%', textAlign: 'center' }} autoplaySpeed={2000}>
-                    <CarouselCard style={{marginTop: '0'}}>
-                        <DiJava size={96} />
-                    </CarouselCard>
-                    <CarouselCard>
-                        <SiSpring size={96}/>
-                    </CarouselCard>
-                    <CarouselCard>
-                        <FaReact size={96} />
-                    </CarouselCard>
-                    <CarouselCard>
-                        <SiTypescript size={96} />
-                    </CarouselCard>
-                    <CarouselCard>
-                        <SiPostgresql size={96} />
-                    </CarouselCard>
-                </Carousel>
-            </Content>
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Carousel pauseOnHover={false} pauseOnFocus={false} autoplay style={{ width: '100%', maxWidth: '400px', textAlign: 'center' }} autoplaySpeed={2000}>
+                            <CarouselCard style={{ marginTop: '0' }}>
+                                <DiJava size={96} />
+                            </CarouselCard>
+                            <CarouselCard>
+                                <SiSpring size={96} />
+                            </CarouselCard>
+                            <CarouselCard>
+                                <FaReact size={96} />
+                            </CarouselCard>
+                            <CarouselCard>
+                                <SiTypescript size={96} />
+                            </CarouselCard>
+                            <CarouselCard>
+                                <SiPostgresql size={96} />
+                            </CarouselCard>
+                        </Carousel>
+                    </div>
+                </Content>
+            </motion.div>
 
             <Content
                 style={{
@@ -105,6 +114,22 @@ export default function Portfolio({ controller }: PortfolioProps) {
                         <p>I'm 17 years old and currently in high school, when i'm not working or studying, I like to develop side projects, and learn new skills.</p>
                     </div>
                 </div>
+
+            </Content>
+
+            <Divider size="large" plain style={{ fontSize: '8px' }}><h1>Skills</h1></Divider>
+
+            <Content
+                style={{
+                    width: '100%',
+                    height: '100vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                id="skills"
+            >
 
             </Content>
 
